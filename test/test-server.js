b28e3c4dd26fdabbea2ad04cfe07f85f2fec3d75
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-10-12 19:39:40 -0700

    Fixed problem with Recipe Put - You don't send a response body back to the client on a put, it should be empty. We are updating information, not sending it to the client

--- test/test-server.js
+++ test/test-server.js
@@ -142,6 +142,41 @@ describe("Shopping List", function() {
   //  5. Inspect the response object to ensure it
   //  has right status code and that we get back an updated
   //  item with the right data in it.
+  it("should update recipes on PUT", function() {
+    // we initialize our updateData here and then after the initial
+    // request to the app, we update it with an `id` property so
+    // we can make a second, PUT call to the app.
+    const updateData = {
+      name: "coffee",
+      ingredients:['cheese', 'meat', 'bun']
+    };
+
+    return (
+      chai
+        .request(app)
+        // first have to get so we have an idea of object to update
+        .get("/recipes")
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          
+          // this will return a promise whose value will be the response
+          // object, which we can inspect in the next `then` block. Note
+          // that we could have used a nested callback here instead of
+          // returning a promise and chaining with `then`, but we find
+          // this approach cleaner and easier to read and reason about.
+          return chai
+            .request(app)
+            .put(`/recipes/${updateData.id}`)
+            .send(updateData);
+        })
+        // prove that the PUT request has right status code
+        // and returns updated item
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+
   it("should update items on PUT", function() {
     // we initialize our updateData here and then after the initial
     // request to the app, we update it with an `id` property so
@@ -179,44 +214,7 @@ describe("Shopping List", function() {
     );
   });
 
-  it("should update recipes on PUT", function() {
-    // we initialize our updateData here and then after the initial
-    // request to the app, we update it with an `id` property so
-    // we can make a second, PUT call to the app.
-    const updateData = {
-      name: "foo",
-      id: "abc",
-      ingredients:"[candy, mango]"
-    };
-
-    return (
-      chai
-        .request(app)
-        // first have to get so we have an idea of object to update
-        .get("/recipes")
-        .then(function(res) {
-          updateData.id = res.body[0].id;
-          // this will return a promise whose value will be the response
-          // object, which we can inspect in the next `then` block. Note
-          // that we could have used a nested callback here instead of
-          // returning a promise and chaining with `then`, but we find
-          // this approach cleaner and easier to read and reason about.
-          return chai
-            .request(app)
-            .put(`/recipes/${updateData.id}`)
-            .send(updateData);
-        })
-        // prove that the PUT request has right status code
-        // and returns updated item
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a("object");
-          expect(res.body).to.deep.equal(updateData);
-        })
-    );
-  });
-
+  
 
 
   // test strategy:
@@ -240,4 +238,21 @@ describe("Shopping List", function() {
   });
 });
 
-//test
+//recipes delete
+
+it("should delete recipes on DELETE", function() {
+  return (
+    chai
+      .request(app)
+      // first have to get so we have an `id` of item
+      // to delete
+      .get("/recipes")
+      .then(function(res) {
+        return chai.request(app).delete(`/recipes/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+      })
+  );
+});
+
